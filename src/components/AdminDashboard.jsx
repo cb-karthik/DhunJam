b7a89bbf73d0ba2c84be875d8606c51d88971d4d
karthik <geokarthikcb@gmail.com>
2023-12-02 16:08:25 +0530

    taoster

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Chart from "./Chart";
 import { useNavigate } from "react-router-dom";
-
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function AdminDashboard() {
   const [chargeForSongs, setChargeForSongs] = useState(false);
@@ -10,8 +10,6 @@ function AdminDashboard() {
   const [amounts, setAmounts] = useState([]);
   const [data, setData] = useState({});
   const navigate = useNavigate();
-
-  const { name, location, charge_customers, display_amount, amount } = data;
 
   useEffect(() => {
     setChargeForSongs(data?.charge_customers);
@@ -23,16 +21,8 @@ function AdminDashboard() {
       data?.amount?.category_9,
       data?.amount?.category_10,
     ];
-
     setAmounts(sortedAmounts);
   }, [data]);
-
- 
-  const handleAmountChange = (index, value) => {
-    const updatedAmounts = [...amounts];
-    updatedAmounts[index] = Number(value); // Make sure we're dealing with numbers not strings
-    setAmounts(updatedAmounts);
-  };
 
   useEffect(() => {
     async function fetchAdminDetails() {
@@ -54,7 +44,6 @@ function AdminDashboard() {
 
         const data = await response.json();
         setData(data?.data);
-        console.log(data);
       } catch (error) {
         console.error("Fetching admin details failed:", error);
       }
@@ -62,17 +51,31 @@ function AdminDashboard() {
     fetchAdminDetails();
   }, []);
 
+  const handleAmountChange = (index, value) => {
+    const updatedAmounts = [...amounts];
+    updatedAmounts[index] = Number(value);
+    setAmounts(updatedAmounts);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const isEnabled =
+      customSongAmount >= 99 &&
+      amounts[0] >= 79 &&
+      amounts[1] >= 59 &&
+      amounts[2] >= 39 &&
+      amounts[3] >= 19;
+
     if (!isEnabled) return;
+
     const url = "https://stg.dhunjam.in/account/admin/4";
     const body = {
       amount: {
         category_6: Number(customSongAmount),
-        category_7: Number(amounts[0]),
-        category_8: Number(amounts[1]),
-        category_9: Number(amounts[2]),
-        category_10: Number(amounts[3])
+        category_7: amounts[0],
+        category_8: amounts[1],
+        category_9: amounts[2],
+        category_10: amounts[3],
       },
     };
 
@@ -85,9 +88,8 @@ function AdminDashboard() {
         body: JSON.stringify(body),
       });
 
-      const result = await response.json();
       if (response.ok) {
-        console.log("Update successful:", result);
+        toast.success("Upadated successfully!");
       } else {
         throw new Error(`Error: ${response.status}`);
       }
@@ -100,17 +102,19 @@ function AdminDashboard() {
     amounts[0] >= 79 &&
     amounts[1] >= 59 &&
     amounts[2] >= 39 &&
-    amounts[3] >= 19
+    amounts[3] >= 19;
 
   return (
-    <div className="min-h-screen h-fit flex flex-col items-center  bg-[#030303] font-poppins text-[#FFFFFF]">
+    <div className="min-h-screen h-fit flex flex-col items-center bg-[#030303] font-poppins text-[#FFFFFF]">
+     
       <div className="text-center text-[#FFFFFF] font-bold text-[32px] mb-4">
-        {name}, {location} on Dhun Jam
+        {data?.name}, {data?.location} on Dhun Jam
       </div>
       <form
-       
         className="w-full max-w-xl px-4 text-[16px]"
+        onSubmit={handleSubmit}
       >
+        {/* Charge for Songs Toggle */}
         <div className="flex items-center justify-between mb-4">
           <label className="text-[16px]  w-[45%]">
             Do you want to charge your customers for requesting songs?
@@ -136,12 +140,14 @@ function AdminDashboard() {
             </label>
           </div>
         </div>
+
+        {/* Custom Song Amount Input */}
         <div
           className={`flex items-center mb-4 ${
             !chargeForSongs ? "opacity-50" : ""
           }`}
         >
-          <label className={`block text-[16px]  w-[45%] mr-3 $`}>
+          <label className={`block text-[16px]  w-[45%] mr-3`}>
             Custom song request amount:
           </label>
           <input
@@ -150,35 +156,33 @@ function AdminDashboard() {
             value={customSongAmount}
             onChange={(e) => setCustomSongAmount(e.target.value)}
             className="flex-1  w-[45%] py-2 px-3 border border-[#FFFFFF] rounded-lg text-[16px] bg-[#030303]"
-            placeholder="444" // or whatever placeholder you want
+            placeholder="Enter value"
           />
         </div>
+
+        {/* Regular Song Amount Inputs */}
         <div className={`flex ${!chargeForSongs ? "opacity-50" : ""}`}>
           <label className="block text-[16px] mr-3 w-[45%]">
-            Regular song request amout from High to low
+            Regular song request amount from High to low
           </label>
           {amounts.map((amount, index) => (
             <input
               disabled={!chargeForSongs}
               key={index}
-              type="text" // Change to text to avoid spinner
-              inputMode="numeric" // Ensures mobile keyboards still give numeric input
-              pattern="[0-9]*" // Ensures only numbers can be input
+              type="text"
+              inputMode="numeric"
+              pattern="[0-9]*"
               value={amount}
               onChange={(e) => handleAmountChange(index, e.target.value)}
               className="w-16 py-2 px-3 border border-white rounded-lg text-[16px] bg-[#030303] mx-1 text-center"
-              style={{
-                // This will hide the spinner from the number input
-                MozAppearance: "textfield",
-                WebkitAppearance: "none",
-              }}
             />
           ))}
         </div>
 
+        {/* Chart Display */}
         {chargeForSongs && (
           <div
-            className={`bg-[#030303] w-[600px] mt-20 h-64 mb-4 flex items-center relative `}
+            className={`bg-[#030303] w-[600px] mt-20 h-64 mb-4 flex items-center relative`}
           >
             <span className="absolute text-white text-xl -top-6 left-0 ">
               ₹
@@ -186,10 +190,10 @@ function AdminDashboard() {
             <Chart amounts={[customSongAmount, ...amounts]} />
           </div>
         )}
+
+        {/* Save Button */}
         <button
           type="submit"
-          onClick={handleSubmit}
-          disabled={!isEnabled}
           className={`w-full mt-24 text-white font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline ${
             isEnabled ? "bg-[#6741D9]" : "bg-gray-400"
           } ${!chargeForSongs ? "opacity-50" : ""}`}
@@ -200,6 +204,7 @@ function AdminDashboard() {
         >
           Save
         </button>
+        <ToastContainer />
       </form>
     </div>
   );
